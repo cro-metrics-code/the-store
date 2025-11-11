59cdc0f071f3842b39bf7d291bc61873197cb574
ryanzigler <rpzigler@gmail.com>
2025-11-11 15:41:26 -0600

    Fix PROD endpoint

--- lib/getBootstrapData.ts
+++ lib/getBootstrapData.ts
@@ -3,14 +3,19 @@ import type { PosthogBootstrapData } from './getServerBootstrapData';
 
 const BOOTSTRAP_ENDPOINT = '/api/posthog/bootstrap';
 
+const getServerEndpoint = () => {
+  const isProduction = process.env.NODE_ENV === 'production';
+  const baseUrl =
+    isProduction ?
+      env.NEXT_PUBLIC_URL
+    : (env.NEXT_PUBLIC_DEV_URL ?? env.NEXT_PUBLIC_URL);
+
+  return new URL(BOOTSTRAP_ENDPOINT, baseUrl).toString();
+};
+
 export const getBootstrapData = async (): Promise<PosthogBootstrapData> => {
   const endpoint =
-    typeof window === 'undefined' ?
-      new URL(
-        BOOTSTRAP_ENDPOINT,
-        env.NEXT_PUBLIC_DEV_URL ?? env.NEXT_PUBLIC_URL,
-      ).toString()
-    : BOOTSTRAP_ENDPOINT;
+    typeof window === 'undefined' ? getServerEndpoint() : BOOTSTRAP_ENDPOINT;
 
   const response = await fetch(endpoint, {
     credentials: 'include',
